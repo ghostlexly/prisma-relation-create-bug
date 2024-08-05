d9019302c28ae1dbea8f580031640d89b278fd79
ghostlexly <ghostlexly@gmail.com>
2024-08-05 18:51:56 +0200

    change type

--- src/modules/housekeeper-information/housekeeper-information.service.ts
+++ src/modules/housekeeper-information/housekeeper-information.service.ts
@@ -6,9 +6,7 @@ import { DatabaseService } from "src/common/providers/database/database.service"
 export class HousekeeperInformationService {
   constructor(private db: DatabaseService) {}
 
-  async create(params: { data: Prisma.HousekeeperInformationCreateInput }) {
-    const { data } = params;
-
+  async create({ data }: { data: Prisma.HousekeeperInformationCreateInput }) {
     // -- create
     const housekeeperInformation =
       await this.db.prisma.housekeeperInformation.create({
